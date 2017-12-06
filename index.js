afd6e759fe3975ee8ce6addea1cc431fb76c6183
igal nassima <inassima@gmail.com>
2017-12-05 20:02:55 -0500

    added perlin noise to fake data

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var perlin = require('perlin-noise');
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+var noiseIndex = 0;
+var noise = perlin.generatePerlinNoise(480, 480);
+
+var getRandomFromNoise = function(min, max) {
+    if(noiseIndex++ >= noise.length) {
+        noiseIndex = 0;
+    }
+
+    var n = noise[noiseIndex];
+    return n * (max - min) + min;
+};
 
 io.on('connection', function(socket){
 
@@ -14,9 +28,8 @@ io.on('connection', function(socket){
   setInterval(function()
     {
       var d = new Date();
-
       io.emit('data1', {
-        "data" : (Math.random() * (10 - 0) + 0),
+        "data" : getRandomFromNoise(1,5),
         "time" : d.toDateString()
       });
       io.emit('data2', {
@@ -28,7 +41,7 @@ io.on('connection', function(socket){
         "time" : d.toDateString()
       });
     },
-  3000);
+  1000);
 });
 
 http.listen(port, function(){
